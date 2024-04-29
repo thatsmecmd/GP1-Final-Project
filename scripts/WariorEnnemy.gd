5263298c9f80bba28644767634da48971387da02
jay <LE2231057@crc-lennox.qc.ca>
2024-04-29 13:59:24 -0400

    added missing stuff

--- scripts/WariorEnnemy.gd
+++ scripts/WariorEnnemy.gd
@@ -1,23 +1,23 @@
 extends Follower
 
-@export var attack_string: String # name of the attack animation
-@export var timer: Timer
-@export var weapon: Node2D
-@export var player: AnimationPlayer
+@onready var timer: Timer= $Timer
+@onready var weapon: Node2D = $Weapon
+@onready var player: AnimationPlayer = $Weapon/AnimationPlayer
+@onready var weapon_hitbox: Area2D = $Weapon/Area2D
 
 var is_timer_finished: bool = true
-# There must be an attack method
 
 func _process(delta):
+	# If the target is in range, call attack method
 	if is_in_range(target.position - position):
 		attack()
 
 func attack():
+	# If the timer is finished, start it back and perform attack animation
 	if(is_timer_finished):
 		is_timer_finished = false
 		timer.start()
-		player.play("sword_swing")
-		
+		player.play("attack")
 
 
 func _on_timer_timeout():
