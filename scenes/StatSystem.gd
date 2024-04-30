5985dc4d93aabffc6267378637dd98489e21d35e
Tyler Ouellet <LE2171096@crc-lennox.qc.ca>
2024-04-30 16:09:26 -0400

    Added more to the StatSystem and added close range attacks

--- scenes/StatSystem.gd
+++ scenes/StatSystem.gd
@@ -8,21 +8,57 @@ var is_dead = false
 var current_health = 1
 signal dead
 
-
 func take_damage(damage):
-	damage = damage - defense 
-	current_health -= damage
-	print(current_health)
+	var dodge_sim = randi() % 100 + 1
+	print(dodge_sim)
+	if dodge <= dodge_sim:
+		print("hit")
+		damage = damage - defense 
+		if damage < 1:
+			damage = 1
+		current_health -= damage
+		print(current_health)
+	else:
+		print("Dodged!")
+		pass
 func dot():
 	pass
-func ap_attack():
-	pass
-func sure_attack():
-	pass
+func ap_attack(damage):
+	var dodge_sim = randi() % 100 + 1
+	if dodge <= dodge_sim:
+		print("hit")
+		current_health -= damage
+	else:
+		print("Dodged!")
+		pass
+func sure_attack(damage):
+	damage = damage - defense 
+	if damage < 1:
+		damage = 1
+	current_health -= damage
+func inert_attack(damage):
+	current_health -= damage 
+func reduce_attack(reduction):
+	attack = attack - reduction
+func reduce_defense(reduction):
+	defense = defense - reduction
+func reduce_dodge(reduction):
+	dodge = dodge - reduction
+func heal(health):
+	current_health += health
+	if current_health >max_health:
+		current_health = max_health 
+func add_defense(buff):
+	defense += buff
+func add_dodge(buff):
+	dodge += buff
+	#prevent imortality 
+	if dodge >= 100:
+		dodge = 99
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_health = max_health
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
---
+++ scripts/attacks/flurry_of_blades.gd
@@ -0,0 +1,30 @@
+extends Area2D
+var damage = 10
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Abilities.can_flurry_of_blades = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func _on_body_entered(body):
+	if body.has_node("StatSystem") and $".".monitoring:
+		var stats = body.get_node("StatSystem")
+		stats.take_damage(damage)
+	else:
+		return
+
+
+func _on_timer_timeout():
+	if $".".monitoring == true:
+		$".".monitoring = false
+	else:
+		$".".monitoring = true
+
+
+func _on_animated_sprite_2d_animation_finished():
+	self.queue_free()
+	Abilities.can_flurry_of_blades = true
