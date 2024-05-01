fa0872b2be3f57597b506156946de991688feb36
Colin Madore <le2287480@crc-lennox.qc.ca>
2024-05-01 11:18:55 -0400

    fully functioning traps

--- scripts/trap.gd
+++ scripts/trap.gd
@@ -4,27 +4,32 @@ extends Node2D
 @onready var open = $open
 @onready var T_open = $Timer_open
 @onready var T_closed = $Timer_closed
+@onready var trap_area = $open/Area2D
+
+var damage = 3
 
 func _ready():
 	T_closed.start()
 
 func _on_timer_open_timeout():
-	print("test open")
 	closed.visible = true
 	open.visible = false
+	trap_area.monitoring = false
 	T_closed.start()
 
 
 func _on_timer_closed_timeout():
-	print("test close")
 	closed.visible = false
 	open.visible = true
+	trap_area.monitoring = true
 	T_open.start()
 
 
 func _on_area_2d_body_entered(body):
+	print("body entered")
 	if body.has_node("StatSystem"):
+		print("gotten stats")
 		var stats = body.get_node("StatSystem")
-		stats.take_damage(5)
+		stats.take_damage(damage)
 	else:
 		pass
