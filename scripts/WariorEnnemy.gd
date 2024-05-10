d1b44eb1d09afb9d6e677e159bd306d765cb45f4
jay <LE2231057@crc-lennox.qc.ca>
2024-05-09 21:03:19 -0400

    Modified enemies attack animations and sounds. Added busses to manage sound effects on footsteps and attacks

--- scripts/WariorEnnemy.gd
+++ scripts/WariorEnnemy.gd
@@ -1,8 +1,7 @@
 extends Follower
 
 @onready var timer: Timer= $Timer
-@onready var attack_sprite: AnimatedSprite2D = $Attack
-@onready var weapon_hitbox: Area2D = $Attack/Area2D
+@onready var weapon_animation: AnimationPlayer = $Attack/AnimationPlayer
 
 var is_timer_finished: bool = true
 var target_is_player: bool = false # stops the enemy from attacking when once it reached it's target
@@ -18,12 +17,12 @@ func attack():
 	if(is_timer_finished && target_is_player):
 		is_timer_finished = false
 		timer.start()
-		attack_sprite.play("Attack")
-		attack_sprite.visible = true
-		weapon_hitbox.monitoring = true
-		await attack_sprite.animation_finished
-		attack_sprite.visible = false
-		weapon_hitbox.monitoring = false
+		weapon_animation.play("Attack")
+		#attack_sprite.visible = true
+		#weapon_hitbox.monitoring = true
+		#await attack_sprite.animation_finished
+		#attack_sprite.visible = false
+		#weapon_hitbox.monitoring = false
 
 
 func _on_timer_timeout():
