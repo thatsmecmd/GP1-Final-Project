b569181e683d5b18a6b7e02aba2ef7405e61c328
Tyler Ouellet <LE2171096@crc-lennox.qc.ca>
2024-05-13 15:01:24 -0400

    fixed persistent floor scaling

--- scripts/UI/game_over.gd
+++ scripts/UI/game_over.gd
@@ -14,8 +14,10 @@ func _process(delta):
 
 func _on_exit_main_pressed():
 	get_tree().paused = false
+	Global.floor = 0
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_retry_pressed():
 	get_tree().paused = false
+	Global.floor = 0
 	get_tree().change_scene_to_file("res://scenes/character_select.tscn")
