549677f1f34d0df784fc9f8b7449db0a144c5e64
Louis-Philippe <LE2253380@crc-lennox.qc.ca>
2024-05-15 15:23:56 -0400

    Created pause menu

--- scripts/UI/ui.gd
+++ scripts/UI/ui.gd
@@ -3,6 +3,14 @@ extends CanvasLayer
 @onready var player = $".."
 @onready var health_bar = $"HealthBar"
 @onready var greed_bar = $HSlider
+@onready var menu_button = $MenuButton
+@onready var menu = $Menu
+@onready var health_num = $Menu/MenuVbox/MaxHealthLabel/HealthNum
+@onready var defense_num = $Menu/MenuVbox/DefenseLabel/DefenseNum
+@onready var dodge_num = $Menu/MenuVbox/DodgeLabel/DodgeNum
+@onready var attack_num = $Menu/MenuVbox/AttackLabel/AttackNum
+
+
 var game_over_scene = preload("res://Scenes/game_over.tscn")
 var current_greed = 100
 
@@ -15,6 +23,12 @@ func _ready():
 func _process(delta):
 	if !(current_greed == Global.greed):
 		greed_bar.value = Global.greed
+
+func init_menu_stats():
+	health_num.text = str(Global.max_health)
+	defense_num.text = str(Global.defense)
+	dodge_num.text = str(Global.dodge)
+	attack_num.text = str(Global.attack)
 
 func _on_player_player_hit():
 	take_damage.emit()
@@ -32,3 +46,15 @@ func _on_greed_timer_timeout():
 
 func _on_player_player_healed():
 	heal_player.emit()
+
+func _on_menu_button_pressed():
+	init_menu_stats()
+	menu.visible = true
+	get_tree().paused = true
+
+func _on_back_button_pressed():
+	menu.visible = false
+	get_tree().paused = false
+
+func _on_home_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
