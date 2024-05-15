6fb70ee88ab46ea6e3bb48c90f195c4461ad13b5
TylerOuellet <tylerouellet93@gmail.com>
2024-05-15 12:59:22 -0400

    Fixed tutorial bug where player is killable after leaving

---
+++ scripts/end_door_toutorial.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var closed_door = $closed_door
+@onready var open_door = $open_door
+@onready var enter_area = $open_door/Area2D
+@onready var light: DirectionalLight2D = $DirectionalLight2D
+@onready var sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+func _on_key_collectible_key_collect():
+	open_door.visible = true
+	closed_door.visible = false
+	enter_area.monitoring = true
+
+
+func _on_area_2d_body_entered(body):
+	print("door entered")
+	sound.play()
+	var tween = create_tween()
+	tween.tween_property(light, "energy", 2, 0.1)
+	await tween.finished
+	await sound.finished
+	get_tree().change_scene_to_file("res://scenes/character_select.tscn")
+	
